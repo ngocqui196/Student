9d5c66cedf1fc363f6e82cabeb1078d6c5084117
ngocqui1996 <hivongmotloihua@yahoo.com>
2020-07-27 22:05:29 +0700

    stuedent

---
+++ src/student.java
@@ -0,0 +1,17 @@
+public class student {
+    public static void main(String[] args) {
+        int a = 10;
+        float f = 20.5f;
+        double d = 120.5;
+        boolean b = true;
+        char s = 'q';
+        String h = "Qui";
+        System.out.println("a = " + a);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("s = " + s);
+        System.out.println("s = " + h);
+
+    }
+}
